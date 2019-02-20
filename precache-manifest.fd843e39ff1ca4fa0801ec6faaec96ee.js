341cf95b0898a719620ad899ec0bd929c5a56a5f
elias-ela <eliasfeleke5@gmail.com>
2019-02-20 13:42:22 +0300

    Updates

---
+++ precache-manifest.fd843e39ff1ca4fa0801ec6faaec96ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3131871cf1a4cc6ecbf0",
+    "url": "/React-Todo-App/static/js/runtime~main.3131871c.js"
+  },
+  {
+    "revision": "02235c2ba6c9e7a46a12",
+    "url": "/React-Todo-App/static/js/main.02235c2b.chunk.js"
+  },
+  {
+    "revision": "ec475ee3870dc2cad1ea",
+    "url": "/React-Todo-App/static/js/2.ec475ee3.chunk.js"
+  },
+  {
+    "revision": "ec475ee3870dc2cad1ea",
+    "url": "/React-Todo-App/static/css/2.c19bb3cf.chunk.css"
+  },
+  {
+    "revision": "a6c4ffaa191189f180fb4655def7b874",
+    "url": "/React-Todo-App/index.html"
+  }
+];
